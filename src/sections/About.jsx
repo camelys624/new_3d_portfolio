de4aa6f1d17dbc1b0a3de8df0d22f105f6929d0b
robbie <robbie.yang@hp.com>
2024-09-23 08:37:18 +0000

    feat: about section done

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,6 +1,20 @@
-import Globe from "react-globe.gl";
+import { useState } from "react"
+import Globe from "react-globe.gl"
+import Button from "../components/Button"
 
 const About = () => {
+  const [hasCopied, setHasCopied] = useState(false)
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText("www.camel_yangz@163.com")
+
+    setHasCopied(true)
+
+    setTimeout(() => {
+      setHasCopied(false)
+    }, 2000)
+  }
+
   return (
     <section className="c-space my-20">
       <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
@@ -46,15 +60,61 @@ const About = () => {
                 width={326}
                 backgroundColor="rgba(0, 0, 0, 0)"
                 backgroundImageOpacity={0.5}
-                globeImageUrl="//unpkg.com/three-globe/example/img/earth-day.jpg"
+                globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
                 bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
               />
+            </div>
+            <div>
+              <p className="grid-headtext">
+                I work remotely across most time zones.
+              </p>
+              <p className="grid-subtext">
+                I'm based in Croatia, with remote work available.
+              </p>
+              <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
+            </div>
+          </div>
+        </div>
+        <div className="xl:col-span-2 xl:row-span-3">
+          <div className="grid-container">
+            <img
+              src="/assets/grid3.png"
+              alt="grid-3"
+              className="w-full sm:h-[266px] h-fit object-contain"
+            />
+            <div>
+              <p className="grid-headtext">My Passion for Coding</p>
+              <p className="grid-subtext">
+                I love solving problems and building things through code. Coding
+                isn't just my profession - it is my passion.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="xl:col-span-1 xl:row-span-2">
+          <div className="grid-container">
+            <img
+              src="/assets/grid4.png"
+              alt="grid-4"
+              className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"
+            />
+            <div className="space-y-2">
+              <p className="grid-subtext text-center">Contact me</p>
+              <div className="copy-container" onClick={handleCopy}>
+                <img
+                  src={hasCopied ? "/assets/tick.svg" : "/assets/copy.svg"}
+                  alt="copy"
+                />
+                <p className="text-xl font-medium text-gray_gradient text-white">
+                  www.camel_yangz@163.com
+                </p>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default About;
+export default About
